8c19f7c65650dd140ed0cde18d8c827e49a73ada
DESKTOP-5RTJJSJ\tejug <tejugowda14@gmail.com>
2020-08-29 14:19:37 -0400

    adding templates for calculator

---
+++ index.js
@@ -0,0 +1,31 @@
+function onReset(id){
+    console.log(id);
+}
+
+function onNumberClick(id){
+    console.log(id);
+}
+
+function onOperationClick(id){
+    console.log(id);
+}
+
+function onModulus(id){
+    console.log(id);
+}
+
+function onSignChange(id){
+    console.log(id);
+}
+
+function onDot(id){
+    console.log(id);
+}
+
+function onDelete(id){
+    console.log(id);
+}
+
+function onequalsClick(id){
+    console.log(id);
+}
